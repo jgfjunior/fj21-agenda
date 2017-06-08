2bb5b819c37fb1ff4d4e4029ec66fc7587e6ce36
web6875 <web6875@caelum55-16>
2017-06-07 22:44:02 -0300

    Inicializacao parametros @WebServlet()

--- fj-21agenda/src/br/com/caelum/servlet/OiMundo.java
+++ fj-21agenda/src/br/com/caelum/servlet/OiMundo.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/oi")
 public class OiMundo extends HttpServlet{
 	
 	@Override
@@ -21,7 +23,7 @@ public class OiMundo extends HttpServlet{
 		out.println("<title>Primeira Servlet</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>Oi Mundo!!!</h1>");
+		out.println("<h1>Oi Mundo Servlet!!!</h1>");
 		out.println("</body>");
 		out.println("</html>");
 		
